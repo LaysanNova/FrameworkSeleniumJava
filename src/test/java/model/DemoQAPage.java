4855fd42e3d318f85e4734f5b2a140b2621bb55f
Laysan Nova <laysansh@gmail.com>
2024-03-12 13:00:03 -0600

    clean

--- src/test/java/model/DemoQAPage.java
+++ src/test/java/model/DemoQAPage.java
@@ -4,10 +4,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.PageFactory;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class DemoQAPage extends BasePage {
 
@@ -46,4 +44,3 @@ public class DemoQAPage extends BasePage {
                 .toList();
     }
 }
-
